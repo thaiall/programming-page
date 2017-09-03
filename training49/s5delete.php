5912fda30c94ccf0fa9cf57be5ad5055d9d3cddb
burin rujjanapan <thaiall@hotmail.com>
2017-09-03 13:46:19 +0700

    Update s5delete.php

--- training49/s5delete.php
+++ training49/s5delete.php
@@ -1,16 +1,20 @@
-<body>
-<form action=s5delete.php>
-<input name=delid value=1001>
-<input type=submit value=s5delete.php>
-</form>
 <?php
+$myForm = '<body>
+<form action="s5delete.php">
+<input name="delid" value="1001">
+<input type="submit" value="s5delete.php">
+</form>';
+if (!isset($_GET['delid'])) { 
+  echo $myForm;
+  exit;
+}
 require("s1connect.php");
-if (!isset($_GET['delid'])) { exit; }
-$sql="delete from worker ";
+$sql="delete from $tb ";
 $sql.="where eid ='".$_GET['delid']."'";
 if(!$result=mysql_db_query($db,$sql))
-echo "$sql : not found";
-else echo "$sql : ok";
+  echo "$sql : failed";
+else 
+  header("location: s0index.php");
 mysql_close($connect);
 ?>
 </body>
