ed2500a3a58e7af5c3e108dff59633d5d27bcd1f
burin rujjanapan <thaiall@hotmail.com>
2017-09-03 13:44:56 +0700

    Update s3select.php

--- training49/s3select.php
+++ training49/s3select.php
@@ -1,13 +1,11 @@
 <?php
-include("s1connect.php");
-$tb="worker";
-echo "Display records : ";
-$query="select * from $tb";
-$result = mysql_db_query($db,$query);
-if ($result) { echo "OK<br/>"; } else { exit; }
+require("s1connect.php");
+$sql="select * from $tb";
+if (!$result=mysql_db_query($db,$sql)) die("Query : failed");
+echo "Display all records : <br/>";
 while ($object = mysql_fetch_object($result)) {
   echo $object->eid . "  " . $object->ename . "<br/>";
 }
-echo "Total records : ".mysql_num_rows($result);
+echo "Total " . mysql_num_rows($result) ." records";
 mysql_close($connect);
 ?>
