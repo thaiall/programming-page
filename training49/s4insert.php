fe6a7e758c0a435e97ac23be164d0855178a1118
burin rujjanapan <thaiall@hotmail.com>
2017-09-03 13:45:43 +0700

    Update s4insert.php

--- training49/s4insert.php
+++ training49/s4insert.php
@@ -1,17 +1,17 @@
 <body>
-<form action=s4insert.php>
-<input name=nid value=1004>
-<input name=nname value=kmit>
-<input type=submit value=s4insert.php>
+<form action="s4insert.php">
+<input name="nid" value="1004">
+<input name="nname" value="kmit">
+<input type="submit" value="s4insert.php">
 </form>
 <?php
+if (!isset($_GET['nid']) || !isset($_GET['nname'])) exit;
 require("s1connect.php");
-if (!isset($_GET['nid']) || !isset($_GET['nname']))
-  exit;
-$sql="insert into worker values('$nid','$nname')";
-if(!$result=mysql_db_query($db,$sql))
-echo "$sql : not found";
-else echo "$sql : ok";
+$sql="insert into $tb values('".$_GET['nid']."','".$_GET['nname']."')";
+if(!$result=mysql_db_query($db,$sql)) 
+  echo "$sql : failed";
+else 
+  echo "$sql : succeeded";
 mysql_close($connect);
 ?>
 </body>
