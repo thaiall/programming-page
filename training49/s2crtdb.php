19b21af751893869e4b9b6312915d7cb49386166
burin rujjanapan <thaiall@hotmail.com>
2017-09-13 14:51:52 +0700

    updated for php7 and php 5 on 2560-09-13

--- training49/s2crtdb.php
+++ training49/s2crtdb.php
@@ -1,4 +1,5 @@
 <?php
+/* updated for php7 and php 5 on 2560-09-13 */
 /* Section 1 : include */
 if(file_exists("s1connect.php"))
   require("s1connect.php");
@@ -6,27 +7,43 @@ else
   die("File not found");
 
 /* Section 2 : main activity */
-dbWork("create database $db");
+if ($dbstatus == 0) dbWork("create database $db");
 qWork("create table $tb (eid char(4),ename char(40))");
 qWork("insert into $tb values('1001','Tom')");
 qWork("insert into $tb values('1002','Dang')");
 qWork("insert into $tb values('1003','Pom')");
-mysql_close($connect);
+if((int)phpversion() >= 7) $connect->close(); else mysql_close($connect);
 echo '<a href="s0index.php">back</a>';
 
 /* Section 3 : function */
 function dbWork($sql) {
-  global $connect;
-  if (!$result=mysql_query($sql,$connect))
-    echo "$sql : failed<br/>";
-  else
-    echo "$sql : succeeded<br/>";
-}
+	global $db,$host,$uname,$upass,$connect;	
+	if((int)phpversion() >= 7) {
+		if ($connect->query($sql) === FALSE) 
+			echo "$sql : failed ". $conn->error . "<br/>";
+		else {
+			echo "$sql : succeeded<br/>";
+			$connect = new mysqli($host, $uname, $upass, $db);
+		}
+	} else { 
+		if (!$result=mysql_query($sql,$connect))
+			echo "$sql : failed<br/>"; 
+		else 
+			echo "$sql : succeeded<br/>";  
+	}
+}	
 function qWork($sql) {
-  global $db;
-  if (!$result=mysql_db_query($db,$sql))
-    echo "$sql : failed<br/>";
-  else
-    echo "$sql : succeeded<br/>";
+	global $connect,$db;
+	if((int)phpversion() >= 7) {
+		if ($connect->query($sql) === FALSE) 
+			echo "$sql : failed ". $conn->error . "<br/>";
+		else 
+			echo "$sql : succeeded<br/>";
+	} else {   
+		if (!$result=mysql_db_query($db,$sql))
+			echo "$sql : failed<br/>"; 
+		else 
+			echo "$sql : succeeded<br/>";  
+	}
 }
 ?>
