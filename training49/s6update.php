22309fd5ad6fa163b8fcc969b5e6fadc81bc451d
burin rujjanapan <thaiall@hotmail.com>
2017-09-13 14:57:20 +0700

    updated for php7 and php 5 on 2560-09-13

--- training49/s6update.php
+++ training49/s6update.php
@@ -1,19 +1,29 @@
 <body>
-<form action=s6update.php>
-<input name=updid value=1003>
-<input name=updename value=chula>
-<input type=submit value=s6update.php>
+<form action="s6update.php">
+<input name="updid" value="1003">
+<input name="updname" value="chula">
+<input type="submit" value="s6update.php">
 </form>
 <?php
-require("s1connect.php");
+/* updated for php7 and php 5 on 2560-09-13 */
 if (!isset($_GET['updid'])) { exit; }
-$sql="update worker set ";
+require("s1connect.php");
+$sql="update $tb set ";
 $sql.="eid='". $_GET['updid'] ."', ";
-$sql.="ename='". $_GET['updename'] ."' ";
+$sql.="ename='". $_GET['updname'] ."' ";
 $sql.="where eid='". $_GET['updid'] ."'";
-if(!$result=mysql_db_query($db,$sql))
-echo "$sql : not found";
-else echo "$sql : ok";
-mysql_close($connect);
+if((int)phpversion() >= 7) {
+	if($connect->query($sql) === FALSE) 
+		echo "$sql : failed";
+	else 
+		echo "<meta http-equiv='refresh' content='0; url=s0index.php'/>";	
+	$connect->close();
+} else {	
+	if(!$result=mysql_db_query($db,$sql)) 
+		echo "$sql : failed";
+	else 
+		echo "<meta http-equiv='refresh' content='0; url=s0index.php'/>";
+	mysql_close($connect);
+}
 ?>
 </body>
