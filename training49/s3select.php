a50eae6600ca9ba7247372e2615a8d5a0b2dd78b
burin rujjanapan <thaiall@hotmail.com>
2017-09-13 14:53:13 +0700

    updated for php7 and php 5 on 2560-09-13

--- training49/s3select.php
+++ training49/s3select.php
@@ -1,13 +1,24 @@
 <?php
-include("s1connect.php");
-$tb="worker";
-echo "Display records : ";
-$query="select * from $tb";
-$result = mysql_db_query($db,$query);
-if ($result) { echo "OK<br/>"; } else { exit; }
-while ($object = mysql_fetch_object($result)) {
-  echo $object->eid . "  " . $object->ename . "<br/>";
+/* updated for php7 and php 5 on 2560-09-13 */
+require("s1connect.php");
+$sql="select * from $tb";
+if((int)phpversion() >= 7) {
+	$result = $connect->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			echo "id: " . $row["eid"]. "- Name: " . $row["ename"]. "<br/>";
+		}
+	} else {
+		echo "no results";
+	}
+	$connect->close();
+} else {	
+	if (!$result=mysql_db_query($db,$sql)) die("Query : failed");
+	echo "Display all records : <br/>";
+	while ($object = mysql_fetch_object($result)) {
+		echo $object->eid . "  " . $object->ename . "<br/>";
+	}
+	echo "Total " . mysql_num_rows($result) ." records";
+	mysql_close($connect);
 }
-echo "Total records : ".mysql_num_rows($result);
-mysql_close($connect);
 ?>
